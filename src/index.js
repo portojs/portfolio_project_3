5ade76102e0a4833832d628e3e4e1b956a4b7519
peter <peter.k.nets@gmail.com>
2016-04-26 11:22:47 +0300

    Add more bg-colors, try radial-gradient and remove hover action

--- src/index.js
+++ src/index.js
@@ -7,41 +7,52 @@ class Container extends React.Component {
 
   render() {
 
-    var textCircle = $("#circle-text"),
-        colors = ["#E64A19", "#FF5722", "#FFCCBC", "#795548",
-      "#727272", "#D32F2F", "#F44336", "#FF4081", "#7B1FA2",
-    "#9C27B0", "#9C27B0", "#E1BEE7"];
+    var colors = ["#E64A19", "#FF5722", "#FFCCBC", "#795548",
+                  "#727272", "#D32F2F", "#F44336", "#FF4081",
+                  "#7B1FA2", "#9C27B0", "#E1BEE7", "#7C4DFF",
+                  "#303F9F", "#3F51B5", "#C5CAE9", "#448AFF",
+                  "#0288D1", "#03A9F4", "#00BCD4", "#009688",
+                  "#B2DFDB"];
 
     function bam() {
-      $("#circle-bg").addClass("enlarge");
+      $(".circle-bg").addClass("enlarge");
     }
-,'#FFCCBC',
+
     function babam() {
-      $("#circle-bg").removeClass("enlarge");
+      $(".circle-bg").removeClass("enlarge");
     }
 
     function disappear() {
-      var el = $("#circle-bg"),
-          newElement = el.clone(true);
+      var circleBg = $(".circle-bg"),
+          newElement = circleBg.clone(true);
 
-      el.addClass("circle-click");
+      circleBg.addClass("circle-click");
       window.setTimeout(function() {
-        el.remove();
+        circleBg.remove();
         $("#wrapper").append(newElement);
-      }, 600);
+        $("body").css({
+          "background-color": "#E1BEE7"
+        });
+      }, 900);
+    }
+
+    window.onload = function() {
+      $(".circle-bg").css(
+        {"background-color": "#E1BEE7"}
+      );
     }
 
     return (
       <div id="wrapper">
 
-        <div id="circle-text" onMouseEnter={bam} onMouseLeave={babam} >
+        <div id="circle-text" >
           <div id="inner">
             <div id="quote-text">Whaaat?</div>
             <button onClick={disappear} id="next-quote">Next Quote</button>
           </div>
         </div>
 
-        <div id="circle-bg">
+        <div className="circle-bg">
         </div>
 
       </div>
