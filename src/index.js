b7e296864b08572f4f9080a2fc20954176c0caef
peter <peter.k.nets@gmail.com>
2016-04-27 12:33:29 +0300

    Implement API

--- src/index.js
+++ src/index.js
@@ -17,18 +17,7 @@ class Container extends React.Component {
         slicedColor = "",
         timeOut;
 
-    // helper function: convert rgb to hex
-    // (from http://www.sitepoint.com/jquery-convert-rgb-hex-color/)
-    // function rgb2hex(rgb) {
-    //   console.log(rgb);
-    //   rgb = rgb.match(/^rgb((d+),s*(d+),s*(d+))$/);
-    //   return "#" +
-    //     ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
-    //     ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
-    //     ("0" + parseInt(rgb[3],10).toString(16)).slice(-2);
-    // }
-
-    function randomBgColor() {
+    function randomColor() {
       var filteredColors, randomNumber;
       if (slicedColor) {
         filteredColors = colors.filter(function(color) {
@@ -40,22 +29,18 @@ class Container extends React.Component {
         randomNumber = Math.floor(Math.random() * (colors.length));
         slicedColor = colors[randomNumber];
       }
-      console.log("Original colors: " + colors);
-      console.log("Filtered colors: " + filteredColors);
-      console.log("Sliced color: " + slicedColor);
       return slicedColor;
     }
 
-    function handleClick() {
+    function animateBgColor() {
       var $circleBg = $(".circle-bg"),
           $button = $("#next-quote"),
           newElement = $circleBg.clone(true),
           oldColor = $circleBg.css("background-color");
 
-      // console.log($(".circle-bg").css("background-color"));
       clearTimeout(timeOut);
       $button.attr("disabled", "disabled");
-      newElement.css({"background-color": randomBgColor()});
+      newElement.css({"background-color": randomColor()});
       $circleBg.addClass("circle-click");
 
       timeOut = window.setTimeout(function() {
@@ -64,17 +49,29 @@ class Container extends React.Component {
         $("body").css({
           "background-color": oldColor
         });
-        // console.log($("body").css("background-color"));
         $button.removeAttr("disabled");
       }, 700);
     }
 
+    function generateQuote() {
+      $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
+        $("#quote-text").html(data.quoteText);
+        data.quoteAuthor ? $("#quote-author").html("-- " + data.quoteAuthor) : $("#quote-author").html("");
+      })
+    }
+
+    function handleClick() {
+      animateBgColor();
+      generateQuote();
+    }
+
     window.onload = function() {
+      generateQuote();
       $("body").css({
-        "background-color": randomBgColor()
+        "background-color": randomColor()
       });
       $(".circle-bg").css({
-        "background-color": randomBgColor()
+        "background-color": randomColor()
       });
     }
 
@@ -84,6 +81,7 @@ class Container extends React.Component {
         <div id="circle-text" >
           <div id="inner">
             <div id="quote-text">Whaaat?</div>
+            <div id="quote-author"></div>
             <button onClick={handleClick} id="next-quote">Next Quote</button>
           </div>
         </div>
